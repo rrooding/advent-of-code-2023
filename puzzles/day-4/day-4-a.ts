53453cee13b304919def950a545911806acefe05
Ralph Rooding <ralph@ralphrooding.com>
2023-12-04 08:39:14 +0100

    day 4

--- puzzles/day-4/day-4-a.ts
+++ puzzles/day-4/day-4-a.ts
@@ -1,8 +1,35 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+type Card = {
+  id: number;
+  winners: number[];
+  numbers: number[];
+};
+
+const parseCard = (line: string): Card => {
+  const [, id, winners, numbers] = line.match(/Card\s+(?<id>\d+):\s+(?<winners>(?:\d+\s*)+) \|\s+(?<numbers>(?:\d+\s+)+\d+)$/m)
+  return { id: parseInt(id), winners: winners.split(/\s+/).map((n) => parseInt(n)), numbers: numbers.split(/\s+/).map((n) => parseInt(n)) };
+};
+
+const pointsForCard = ({ winners, numbers }): number =>
+  numbers.reduce((score: number, number: number) =>
+    winners.includes(number) ? score ? score *= 2 : score = 1 : score
+  , 0);
+
 export async function day4a(dataPath?: string) {
   const data = await readData(dataPath);
+
+  return data
+    .filter(Boolean)
+    .map(parseCard)
+    .map(pointsForCard)
+    .reduce((a, b) => a + b)
+
+  // const points = pointsForCard(parsed[0]);
+  // console.log("points", points)
+
+
   return 0;
 }
 
--- puzzles/day-4/day-4-b.ts
+++ puzzles/day-4/day-4-b.ts
@@ -1,9 +1,50 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+type Card = {
+  id: number;
+  winners: number[];
+  numbers: number[];
+  extraCards?: number[];
+};
+
+const parseCard = (line: string): Card => {
+  const [, id, winners, numbers] = line.match(/Card\s+(?<id>\d+):\s+(?<winners>(?:\d+\s*)+) \|\s+(?<numbers>(?:\d+\s+)+\d+)$/m)
+  return { id: parseInt(id), winners: winners.split(/\s+/).map((n) => parseInt(n)), numbers: numbers.split(/\s+/).map((n) => parseInt(n)) };
+};
+
+const pointsForCard = ({ winners, numbers }): number =>
+  numbers.reduce((score: number, number: number) =>
+    winners.includes(number) ? score += 1 : score
+  , 0);
+
+const findWinners = (card: Card): Card => ({
+  ...card,
+  extraCards: Array.from({length: pointsForCard(card)}, (_, k) => k + card.id + 1),
+});
+
+const scoreForCardFactory = (cards: Card[]) => (card: Card): number => {
+  if(card.extraCards?.length) {
+    const extraCards = card.extraCards.map((eid) => cards.find(({ id }) => id === eid));
+    const nestedScore = extraCards.map(scoreForCardFactory(cards)).reduce((a, b) => a + b);
+    return card.extraCards.length +  nestedScore;
+  }
+  return 0;
+}
+
 export async function day4b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const cards = data
+    .filter(Boolean)
+    .map(parseCard)
+    .map(findWinners)
+
+  const scoreForCard = scoreForCardFactory(cards);
+
+  return cards.reduce((score, card) => {
+    return score + 1 + scoreForCard(card);
+  }, 0)
 }
 
 const answer = await day4b();
