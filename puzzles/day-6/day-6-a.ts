6b5879f071d03abb5b28e206e342c015ffb8fb5d
Ralph Rooding <ralph@ralphrooding.com>
2023-12-06 08:03:50 +0100

    day 6

--- puzzles/day-6/day-6-a.ts
+++ puzzles/day-6/day-6-a.ts
@@ -3,8 +3,17 @@ import chalk from 'chalk';
 
 export async function day6a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const durations = data[0].match(/\d+/g).map(Number);
+  const times = data[1].match(/\d+/g).map(Number);
+
+  return durations.map((d, i) => {
+    const Δ = Math.sqrt((d*d)-(4*times[i]))
+    return Math.floor((d+Δ)/2)-Math.ceil((d-Δ)/2)+1;
+  }).reduce((a,b)=>a*b);
 }
 
 const answer = await day6a();
 console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
+
+  // races.push({ d: parseInt(durations.map((n) => n.toString()).join("")), r: parseInt(records.map((n) => n.toString()).join("")) });
--- puzzles/day-6/day-6-b.ts
+++ puzzles/day-6/day-6-b.ts
@@ -3,7 +3,12 @@ import chalk from 'chalk';
 
 export async function day6b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const d= parseInt(data[0].match(/\d+/g).join(""));
+  const t= parseInt(data[1].match(/\d+/g).join(""));
+
+  const Δ = Math.sqrt((d*d)-(4*t))
+  return Math.floor((d+Δ)/2)-Math.ceil((d-Δ)/2)+1;
 }
 
 const answer = await day6b();
