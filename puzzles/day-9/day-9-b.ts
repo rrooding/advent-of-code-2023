1591d63fc1cbe75bccc23ed72c5f513039e85a90
Ralph Rooding <ralph@ralphrooding.com>
2023-12-09 09:24:07 +0100

    day 9

--- puzzles/day-9/day-9-b.ts
+++ puzzles/day-9/day-9-b.ts
@@ -1,9 +1,17 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
+import { combinations } from "mathjs";
+
+const l = (numbers: number[]) =>
+  numbers.reduce((sum, num, i) => sum+(num*combinations(numbers.length, i + 1)*(-1)**i), 0);
 
 export async function day9b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const data = await readData(dataPath)
+  return data
+    .filter(Boolean)
+    .map((l) => l.split(" ").map(Number))
+    .map(l)
+    .reduce((a,b) => a+b);
 }
 
 const answer = await day9b();
