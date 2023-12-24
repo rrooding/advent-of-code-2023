5c86cd6c75b68f57a3b5c7d00bf1f13f01547c90
Ralph Rooding <ralph@ralphrooding.com>
2023-12-24 10:13:46 +0100

    day 24

--- puzzles/day-24/day-24-a.ts
+++ puzzles/day-24/day-24-a.ts
@@ -1,9 +1,53 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const intersect = (x1: number, y1: number, x2: number, y2: number, x3: number, y3: number, x4: number, y4: number) => {
+  const denom = (y4-y3) * (x2-x1) - (x4-x3) * (y2-y1);
+  if (denom == 0) return null;
+  const ua = ((x4-x3) * (y1-y3) - (y4-y3) * (x1-x3)) / denom;
+  return [x1+ua*(x2-x1), y1+ua*(y2-y1)];
+}
+
 export async function day24a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const stones = data.filter(Boolean).map((l) => {
+    const { groups: { x, y, z, vx, vy, vz } } = l.match(/(?<x>\d+), (?<y>\d+), (?<z>\d+) @ (?<vx>-?\d+), (?<vy>-?\d+), (?<vz>-?\d+)/);
+    return { x:+x, y:+y, z:+z, vx:+vx, vy:+vy, vz:+vz };
+  });
+
+  const cmin = 200000000000000, cmax = 400000000000000;
+
+  let intersecting = 0;
+  for (let s1 = 0; s1 < stones.length; s1++) {
+    for (let s2 = s1 + 1; s2 < stones.length; s2++) {
+      const stone1 = stones[s1], stone2 = stones[s2];
+
+      const intersection = intersect(
+        stone1.x,
+        stone1.y,
+        stone1.x+stone1.vx,
+        stone1.y+stone1.vy,
+        stone2.x,
+        stone2.y,
+        stone2.x+stone2.vx,
+        stone2.y+stone2.vy,
+      );
+
+      if(intersection) {
+        const [x,y] = intersection;
+        if (x > stone1.x === stone1.vx > 0 &&
+            y > stone1.y === stone1.vy > 0 &&
+            x > stone2.x == stone2.vx > 0 &&
+            y > stone2.y === stone2.vy > 0 &&
+            x >= cmin && x <= cmax &&
+            y >= cmin && y <= cmax)
+          intersecting++;
+      }
+    }
+  }
+
+  return intersecting;
 }
 
 const answer = await day24a();
--- puzzles/day-24/day-24-b.ts
+++ puzzles/day-24/day-24-b.ts
@@ -1,9 +1,45 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+import { init as initZ3 } from "z3-solver";
+
 export async function day24b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const stones = data.filter(Boolean).map((l) => {
+    const { groups: { x, y, z, vx, vy, vz } } = l.match(/(?<x>\d+), (?<y>\d+), (?<z>\d+) @ (?<vx>-?\d+), (?<vy>-?\d+), (?<vz>-?\d+)/);
+    return { x:+x, y:+y, z:+z, vx:+vx, vy:+vy, vz:+vz };
+  });
+
+  const { Context, em: { PThread } } = await initZ3();
+  const z3 = Context('main');
+  const solver = new z3.Solver();
+
+  const x = z3.Real.const('x'),
+        y = z3.Real.const('y'),
+        z = z3.Real.const('z'),
+        vx = z3.Real.const('vx'),
+        vy = z3.Real.const('vy'),
+        vz = z3.Real.const('vz');
+
+  let step = 0;
+  for (const stone of stones.slice(0,4)) {
+    const ns = z3.Real.const(`t${step}`);
+    solver.add(ns.gt(z3.Real.val(0)));
+    solver.add(x.add(ns.mul(vx)).eq(z3.Real.val(stone.x).add(ns.mul(z3.Real.val(stone.vx)))));
+    solver.add(y.add(ns.mul(vy)).eq(z3.Real.val(stone.y).add(ns.mul(z3.Real.val(stone.vy)))));
+    solver.add(z.add(ns.mul(vz)).eq(z3.Real.val(stone.z).add(ns.mul(z3.Real.val(stone.vz)))));
+    step++;
+  }
+
+  await solver.check();
+  const model = solver.model();
+
+  const total = [x, y, z].reduce((s, ex) => s+(+model.eval(ex).toString()), 0);
+
+  PThread.terminateAllThreads();
+
+  return total;
 }
 
 const answer = await day24b();
